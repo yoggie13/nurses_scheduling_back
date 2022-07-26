addb1d51750bfb51518ba0aa1c4c6e4a6d06679f
yoggie13 <ognjennikolic13@gmail.com>
2022-07-26 16:31:14 +0200

    test

--- db_connection.js
+++ db_connection.js
@@ -1,7 +1,5 @@
 const path = require('path')
-require('dotenv').config({
-    path: path.resolve(__dirname, '../.env')
-})
+require('dotenv').config()
 var mysql = require("mysql");
 
 console.log(process.env)
