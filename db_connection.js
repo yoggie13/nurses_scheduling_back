34a8ba29e007717c12acf1d84db791ca1869840e
yoggie13 <ognjennikolic13@gmail.com>
2022-07-26 16:24:44 +0200

    created different project node

---
+++ db_connection.js
@@ -0,0 +1,23 @@
+const path = require('path')
+require('dotenv').config({
+    path: path.resolve(__dirname, '../.env')
+})
+var mysql = require("mysql");
+
+console.log(process.env)
+
+var connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+})
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log("Connected to mysql");
+})
+
+module.exports = {
+    GetConnection: function () { return connection }
+}
