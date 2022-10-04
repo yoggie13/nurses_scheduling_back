cd384dcf1b2f5be3be38c18adba51700eabc2b09
Ognjen <ognjennikolic13@gmail.com>
2022-10-04 23:30:53 +0200

    removed unnecessary db_connection module

--- db_connection.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-var mysql = require("mysql2/promise");
-
-module.exports = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
